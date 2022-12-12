f24c47fe15bb5e541e23f0dd19e34f2f53bdd6fb
PyrrhicShadow <jrk135711@gmail.com>
2022-12-11 20:56:11 -0800

    Starting on start/end menus

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.UI; 
+
+public enum GameState {
+    move, pause, end 
+}
 
 public class GameManager : MonoBehaviour {
 
@@ -10,12 +14,22 @@ public class GameManager : MonoBehaviour {
     [SerializeField] int boardSize; 
     [SerializeField] List<Card> _hand; 
     public List<Card> hand { get { return _hand; } private set { _hand = value; } }
+    [SerializeField] GameState _state = GameState.pause; 
+    public GameState state { get { return _state; } private set { _state = value; } }
+    [SerializeField] Image clock; 
+    [SerializeField] Text time; 
     private int matches; 
+    private float timer; 
+    [SerializeField] int timeGoal; 
+    private int counter; 
+
+    [SerializeField] Animator menuAnimator; 
+    [SerializeField] Animator pauseAnimator; 
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         matches = 0; 
+        counter = timeGoal; 
         hand = new List<Card>(); 
         SetUp(); 
     }
@@ -50,13 +64,16 @@ public class GameManager : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
+    void Update() {
+        timer -= Time.deltaTime; 
+        if (timer <= 0) {
+            UpdateTimer(); 
+            timer = 1; 
+        }
     }
 
     public void AddCard(Card newCard) {
-        if (hand.Count < 2) {
+        if (hand.Count < 2 && state == GameState.move) {
             hand.Add(newCard); 
             newCard.Flip(); 
         }
@@ -86,13 +103,55 @@ public class GameManager : MonoBehaviour {
         matches++; 
         if (matches == cardPrefabs.Length) {
             // You win!
-            Debug.Log("You win!"); 
+            WinGame(); 
         }
     }
 
     private void ResetHand() {
         foreach (Card c in hand) {
             c.Reset(); 
+        }
+    }
+
+    [ContextMenu("Play")]
+    public void PlayGame() {
+        // menuAnimator.SetTrigger("play"); 
+        state = GameState.move; 
+    }
+
+    [ContextMenu("Pause")]
+    public void PauseGame() {
+        if (state == GameState.pause) {
+            state = GameState.move; 
+            // pauseAnimator.SetTrigger("unpause");
+        }
+        else {
+            state = GameState.pause; 
+            // pauseAnimator.SetTrigger("pause"); 
+        }
+    }
+
+    private void WinGame() {
+        Debug.Log("You win!"); 
+        // menuAnimator.SetTrigger("win"); 
+        state = GameState.end; 
+    }
+
+    private void LoseGame() {
+        Debug.Log("You Lose :("); 
+        // menuAnimator.SetTrigger("lose"); 
+        state = GameState.end; 
+    }
+
+    private void UpdateTimer() {
+        if (state == GameState.move) {
+            counter--; 
+            clock.fillAmount = (float)counter / (float)timeGoal; 
+            time.text = counter.ToString(); 
+        }
+
+        if (counter <= 0) {
+            LoseGame(); 
         }
     }
 
